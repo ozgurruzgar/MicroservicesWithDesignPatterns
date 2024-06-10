117a28e61d172d92de340a4c4e5f048f6f5ffbd7
ozgurruzgar <ozgurruzgar@users.noreply.github.com>
2024-06-10 15:35:51 +0300

    Almost completed development

---
+++ EventSourcing.API/EventStores/ProductStream.cs
@@ -0,0 +1,35 @@
+﻿using EventSourcing.API.Dtos;
+using EventSourcing.Shared.Events;
+using EventStore.ClientAPI;
+
+namespace EventSourcing.API.EventStores
+{
+    public class ProductStream : AbstractStream
+    {
+        public static string StreamName => "ProductStream";
+
+        public ProductStream(IEventStoreConnection eventStoreConnection) : base(StreamName, eventStoreConnection)
+        {
+        }
+
+        public void Created(CreateProductDto createProductDto)
+        {
+            Events.AddLast(new ProductCreatedEvent { Id = Guid.NewGuid(), Name = createProductDto.Name, Price = createProductDto.Price, Stock = createProductDto.Stock, UserId = createProductDto.UserId });
+        }
+
+        public void Deleted(Guid id)
+        {
+            Events.AddLast(new ProductDeletedEvent { Id = id });
+        }
+
+        public void NameChanged(ChangeProductNameDto changeProductNameDto)
+        {
+            Events.AddLast(new ProductNameChangedEvent { Id = Guid.NewGuid(), ChangedName = changeProductNameDto.Name});
+        }
+
+        public void PriceChanged(ChangeProductPriceDto changeProductPriceDto)
+        {
+            Events.AddLast(new ProductPriceChangedEvent { Id = Guid.NewGuid(), ChangedPrice = changeProductPriceDto.Price });
+        }
+    }
+}
